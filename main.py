c7b989cca36819d5d5e84bf6f10b296c977e2ee0
Dae_Draco_ <DaemoniumDraco@users.noreply.github.com>
2023-10-15 17:40:49 +0100

    Fixed Initilisation of class

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello world")
+class puzzle:
+    def __init__(self, difficulty):
+        self.__grid = [[0 for i in range(9)]for j in range(9)]
+
+    def show_grid(self):
+        for i in self.__grid:
+            for j in i:
+                print(j, end = "  ")
+            print()
+
+    def generate(self):
+        pass
+
+    def validate(self):
+        pass
+
+    def solve(self):
+        pass
+
+
+puzzle1 = puzzle()
+
+puzzle1.show_grid()
